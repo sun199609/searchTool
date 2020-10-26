59264a3484c33bf0274a05135af92d0d1a70da03
sun199609 <32871273+sun199609@users.noreply.github.com>
2020-10-26 16:49:46 -0500

    Update example.py

--- example.py
+++ example.py
@@ -12,30 +12,65 @@ import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
+# Disable OAuthlib's HTTPS verification when running locally.
+# *DO NOT* leave this option enabled in production.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+api_service_name = "youtube"
+api_version = "v3"
+client_secrets_file = "client_secret_463048915584-qrp7qtk37bo3m9gvm5qboho7jb6hhku5.apps.googleusercontent.com.json"
+
+# Get credentials and create an API client
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    client_secrets_file, scopes)
+credentials = flow.run_console()
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
+
+
+
+def get_related_videos(query):
+    videos = []
+    next_page_token = None
+    
+    while 1:
+        res = youtube.search().list(
+            part="snippet",
+            q=query,
+            type="video",
+            maxResults=50,
+            pageToken=next_page_token
+        ).execute()
+        videos += res['items']
+        next_page_token = res.get('nextPageToken')
+        if next_page_token is None:
+            break
+     
+    videos = sorted(videos, key=lambda x:x['snippet']['publishedAt'])
+    
+    return videos
+
+
+def get_videos_stats(video_ids):
+    stats = []
+    for i in range(0, len(video_ids), 50):
+        res = youtube.videos.list(id=','.join(video_ids[i:i+50]),
+                                 part='statistics').execute()
+        stats += res['items']
+    return stats
+
+
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret_463048915584-qrp7qtk37bo3m9gvm5qboho7jb6hhku5.apps.googleusercontent.com.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    request = youtube.search().list(
-        part="snippet",
-        q="tenet",
-        type="video"
-    )
-    response = request.execute()
-
-    print(response)
+    
+    # get related videos
+    videos = get_related_videos("tenet")
+  
+    # get video stats
+    video_ids = list(map(lambda x:x['id']['videoId'], videos))
+    stats = get_videos_stats(video_ids)
+    println(len(stats))
+    
+    
 
 if __name__ == "__main__":
     main()
