daccb096d793bd43e31d79bb580dee569299efd5
sun199609 <32871273+sun199609@users.noreply.github.com>
2020-10-26 23:28:37 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Sample Python code for youtube.search.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
-
 import os
 import csv
 import json
